68714840806a71a4af72a637af880b72a229eebc
asmwriter <ganeshece1317@gmail.com>
2023-01-26 20:36:34 -0500

    trying 5.4

--- prog5_saxpy/saxpyStreaming.cpp
+++ prog5_saxpy/saxpyStreaming.cpp
@@ -17,6 +17,18 @@ void saxpyStreaming(int N,
                     float result[])
 {
     // Replace this code with ones that make use of the streaming instructions
-    saxpySerial(N, scale, X, Y, result);
+    __m128 vec_scale = _mm_broadcast_ss (&scale);
+    long unsigned int vec_alignment  = 128;
+    long unsigned int vec_width(vec_alignment/sizeof(float));
+	long unsigned int nbVec = (N/vec_width);
+    //saxpySerial(N, scale, X, Y, result);
+    for (int i=0; i<nbVec; i++) {
+        //result[i] = scale * X[i] + Y[i];
+        __m128 vec_X = _mm_stream_load_si128(X + i*vec_width);
+		__m128 vec_aX = _mm_mul_ps (vecX, vec_scale);
+		__m128 vec_Y = _mm_stream_load_si128(Y + i*vec_width);
+		__m128 vec_result = _mm_add_ps(vecAX, vecY);
+		_mm_store_ps(result + i*vec_width, vec_result);
+    }
 }
 
