d64d637a9d1d4cc73b29d30d569ffc1861421ffe
asmwriter <ganeshece1317@gmail.com>
2023-01-26 21:09:52 -0500

    casting to _m128*

--- prog5_saxpy/saxpyStreaming.cpp
+++ prog5_saxpy/saxpyStreaming.cpp
@@ -1,5 +1,6 @@
 #include <smmintrin.h> // For _mm_stream_load_si128
 #include <emmintrin.h> // For _mm_mul_ps
+#include <immintrin.h>
 #include <assert.h>
 #include <stdint.h>
 
@@ -19,16 +20,16 @@ void saxpyStreaming(int N,
     // Replace this code with ones that make use of the streaming instructions
     __m128 vec_scale = _mm_broadcast_ss (&scale);
     long unsigned int vec_alignment  = 128;
-    long unsigned int vec_width(vec_alignment/sizeof(float));
+    long unsigned int vec_width = vec_alignment/sizeof(float);
 	long unsigned int nbVec = (N/vec_width);
     //saxpySerial(N, scale, X, Y, result);
     for (int i=0; i<nbVec; i++) {
-        //result[i] = scale * X[i] + Y[i];
-        __m128 vec_X = _mm_stream_load_si128(X + i*vec_width);
-		__m128 vec_aX = _mm_mul_ps (vecX, vec_scale);
-		__m128 vec_Y = _mm_stream_load_si128(Y + i*vec_width);
-		__m128 vec_result = _mm_add_ps(vecAX, vecY);
-		_mm_store_ps(result + i*vec_width, vec_result);
+        //result[i] = scale * X[i] + Y[i];        
+        __m128 vec_X = _mm_stream_load_si128((__m128i*)(X + i*vec_width));
+		__m128 vec_aX = _mm_mul_ps (vec_X, vec_scale);
+		__m128 vec_Y = _mm_stream_load_si128((__m128i*)(Y + i*vec_width));
+		__m128 vec_result = _mm_add_ps(vec_aX, vec_Y);
+		_mm_stream_ps (result + i*vec_width, vec_result);
     }
 }
 
